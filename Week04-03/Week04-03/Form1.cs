54ae48eeaabadbb8823fdbd4382b0c77ad013a22
Vic <h88001166@gmail.com>
2023-03-15 17:50:05 +0800

    Judge by the leap year.

---
+++ Week04-03/Week04-03/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week04_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int year = Convert.ToInt32(textBox1.Text);
+            string result = "";
+
+            if (year % 4 != 0) {
+                result = "Common Year";
+            }
+            else
+            {
+                if (year % 400 == 0)
+                {
+                    result = "Leap Year";
+                }
+                else if (year % 100 == 0)
+                {
+                    result = "Common Year";
+                }
+                else
+                {
+                    result = "Leap Year";
+                }
+            }
+
+            label1.Text = result;
+        }
+    }
+}
