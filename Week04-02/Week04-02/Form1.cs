2bf29b3f785bd72e37a15e9b309874df25b6954e
Vic <h88001166@gmail.com>
2023-03-15 16:54:46 +0800

    Calculate the tax.

--- Week04-02/Week04-02/Form1.cs
+++ Week04-02/Week04-02/Form1.cs
@@ -16,5 +16,34 @@ namespace Week04_02
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int income = Convert.ToInt32(textBox1.Text);
+            double tax = 0;
+
+            if (income < 500000)
+            {
+                tax = income * 0.05;
+            }
+            else if (income < 1000000)
+            {
+                tax = income * 0.1 - 25000;
+            }
+            else if (income < 2000000)
+            {
+                tax = income * 0.2 - 125000;
+            }
+            else if (income < 4000000)
+            {
+                tax = income * 0.3 - 325000;
+            }
+            else
+            {
+                tax = income * 0.4 - 625000;
+            }
+
+            label2.Text = "Tax: " + tax.ToString("N");
+        }
     }
 }
