7342641353c8eef1c0127ba9d8b051b51dd77a30
Vic <h88001166@gmail.com>
2023-03-22 16:49:40 +0800

    Get six lottery numbers.

--- Week05-01/Week05-01/Form1.cs
+++ Week05-01/Week05-01/Form1.cs
@@ -16,5 +16,34 @@ namespace Week05_01
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] lotteryNumbers = new int[6];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                int lotteryNumber = rnd.Next(1, 49);
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (lotteryNumber == lotteryNumbers[j])
+                    {
+                        lotteryNumber = rnd.Next(1, 49);
+                        j = 0;
+                    }
+                }
+
+                lotteryNumbers[i] = lotteryNumber;
+            }
+
+            textBox1.Text = "";
+            foreach (var item in lotteryNumbers)
+            {
+                textBox1.Text += item + " ";
+            }
+        }
     }
 }
