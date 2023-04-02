cc98ee113b412b7fa829e0d410fde42b3f708cc4
Vic <h88001166@gmail.com>
2023-04-02 13:36:42 +0800

    Make a scratch-off lottery simulator.

--- Homework01/Homework01/Form1.Designer.cs
+++ Homework01/Homework01/Form1.Designer.cs
@@ -28,12 +28,312 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            labelY1 = new Label();
+            labelY2 = new Label();
+            labelY3 = new Label();
+            labelY4 = new Label();
+            labelY5 = new Label();
+            labelP5 = new Label();
+            labelP1 = new Label();
+            labelO2 = new Label();
+            labelP2 = new Label();
+            labelO3 = new Label();
+            labelP3 = new Label();
+            labelO4 = new Label();
+            labelP4 = new Label();
+            labelO5 = new Label();
+            labelO1 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(45, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Scratch";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(45, 147);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Scratch";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(45, 202);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Scratch";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(45, 257);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 3;
+            button4.Text = "Scratch";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(45, 312);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 4;
+            button5.Text = "Scratch";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(199, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Your Number";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(356, 42);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Opponent's Nunber";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(547, 42);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Prize";
+            // 
+            // labelY1
+            // 
+            labelY1.AutoSize = true;
+            labelY1.Location = new Point(232, 96);
+            labelY1.Name = "labelY1";
+            labelY1.Size = new Size(12, 15);
+            labelY1.TabIndex = 8;
+            labelY1.Text = "-";
+            // 
+            // labelY2
+            // 
+            labelY2.AutoSize = true;
+            labelY2.Location = new Point(232, 151);
+            labelY2.Name = "labelY2";
+            labelY2.Size = new Size(12, 15);
+            labelY2.TabIndex = 11;
+            labelY2.Text = "-";
+            // 
+            // labelY3
+            // 
+            labelY3.AutoSize = true;
+            labelY3.Location = new Point(232, 206);
+            labelY3.Name = "labelY3";
+            labelY3.Size = new Size(12, 15);
+            labelY3.TabIndex = 14;
+            labelY3.Text = "-";
+            // 
+            // labelY4
+            // 
+            labelY4.AutoSize = true;
+            labelY4.Location = new Point(232, 261);
+            labelY4.Name = "labelY4";
+            labelY4.Size = new Size(12, 15);
+            labelY4.TabIndex = 17;
+            labelY4.Text = "-";
+            // 
+            // labelY5
+            // 
+            labelY5.AutoSize = true;
+            labelY5.Location = new Point(232, 316);
+            labelY5.Name = "labelY5";
+            labelY5.Size = new Size(12, 15);
+            labelY5.TabIndex = 20;
+            labelY5.Text = "-";
+            // 
+            // labelP5
+            // 
+            labelP5.AutoSize = true;
+            labelP5.Location = new Point(557, 316);
+            labelP5.Name = "labelP5";
+            labelP5.Size = new Size(12, 15);
+            labelP5.TabIndex = 22;
+            labelP5.Text = "-";
+            // 
+            // labelP1
+            // 
+            labelP1.AutoSize = true;
+            labelP1.Location = new Point(557, 96);
+            labelP1.Name = "labelP1";
+            labelP1.Size = new Size(12, 15);
+            labelP1.TabIndex = 10;
+            labelP1.Text = "-";
+            // 
+            // labelO2
+            // 
+            labelO2.AutoSize = true;
+            labelO2.Location = new Point(406, 151);
+            labelO2.Name = "labelO2";
+            labelO2.Size = new Size(12, 15);
+            labelO2.TabIndex = 12;
+            labelO2.Text = "-";
+            // 
+            // labelP2
+            // 
+            labelP2.AutoSize = true;
+            labelP2.Location = new Point(557, 151);
+            labelP2.Name = "labelP2";
+            labelP2.Size = new Size(12, 15);
+            labelP2.TabIndex = 13;
+            labelP2.Text = "-";
+            // 
+            // labelO3
+            // 
+            labelO3.AutoSize = true;
+            labelO3.Location = new Point(406, 206);
+            labelO3.Name = "labelO3";
+            labelO3.Size = new Size(12, 15);
+            labelO3.TabIndex = 15;
+            labelO3.Text = "-";
+            // 
+            // labelP3
+            // 
+            labelP3.AutoSize = true;
+            labelP3.Location = new Point(557, 206);
+            labelP3.Name = "labelP3";
+            labelP3.Size = new Size(12, 15);
+            labelP3.TabIndex = 16;
+            labelP3.Text = "-";
+            // 
+            // labelO4
+            // 
+            labelO4.AutoSize = true;
+            labelO4.Location = new Point(406, 261);
+            labelO4.Name = "labelO4";
+            labelO4.Size = new Size(12, 15);
+            labelO4.TabIndex = 18;
+            labelO4.Text = "-";
+            // 
+            // labelP4
+            // 
+            labelP4.AutoSize = true;
+            labelP4.Location = new Point(557, 261);
+            labelP4.Name = "labelP4";
+            labelP4.Size = new Size(12, 15);
+            labelP4.TabIndex = 19;
+            labelP4.Text = "-";
+            // 
+            // labelO5
+            // 
+            labelO5.AutoSize = true;
+            labelO5.Location = new Point(406, 316);
+            labelO5.Name = "labelO5";
+            labelO5.Size = new Size(12, 15);
+            labelO5.TabIndex = 21;
+            labelO5.Text = "-";
+            // 
+            // labelO1
+            // 
+            labelO1.AutoSize = true;
+            labelO1.Location = new Point(406, 96);
+            labelO1.Name = "labelO1";
+            labelO1.Size = new Size(12, 15);
+            labelO1.TabIndex = 9;
+            labelO1.Text = "-";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(45, 373);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 15);
+            label4.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(labelP5);
+            Controls.Add(labelO5);
+            Controls.Add(labelY5);
+            Controls.Add(labelP4);
+            Controls.Add(labelO4);
+            Controls.Add(labelY4);
+            Controls.Add(labelP3);
+            Controls.Add(labelO3);
+            Controls.Add(labelY3);
+            Controls.Add(labelP2);
+            Controls.Add(labelO2);
+            Controls.Add(labelY2);
+            Controls.Add(labelP1);
+            Controls.Add(labelO1);
+            Controls.Add(labelY1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label labelY1;
+        private Label labelY2;
+        private Label labelY3;
+        private Label labelY4;
+        private Label labelY5;
+        private Label labelP5;
+        private Label labelP1;
+        private Label labelO2;
+        private Label labelP2;
+        private Label labelO3;
+        private Label labelP3;
+        private Label labelO4;
+        private Label labelP4;
+        private Label labelO5;
+        private Label labelO1;
+        private Label label4;
     }
 }
--- Homework01/Homework01/Form1.cs
+++ Homework01/Homework01/Form1.cs
@@ -6,5 +6,77 @@ namespace Homework01
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            scratch(labelY1, labelO1, labelP1, button1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            scratch(labelY2, labelO2, labelP2, button2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            scratch(labelY3, labelO3, labelP3, button3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            scratch(labelY4, labelO4, labelP4, button4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            scratch(labelY5, labelO5, labelP5, button5);
+        }
+
+        Random rnd = new Random();
+        int[] prizes = { 100, 1000, 10000, 100000 };
+        int total = 0;
+
+        private void scratch(Label yourLabel, Label opponentLabel, Label prizeLabel, Button scratchButton)
+        {
+            int yourNumber = rnd.Next(20, 30);
+            int opponentNumber;
+            do
+            {
+                opponentNumber = rnd.Next(20, 30);
+            } while (yourNumber == opponentNumber);
+            int tempPrizeIndex = rnd.Next(100);
+            int prizeIndex = 0;
+            switch (tempPrizeIndex)
+            {
+                case < 75:
+                    prizeIndex = 0;
+                    break;
+                case < 95:
+                    prizeIndex = 1;
+                    break;
+                case < 99:
+                    prizeIndex = 2;
+                    break;
+                case < 100:
+                    prizeIndex = 3;
+                    break;
+            }
+
+            yourLabel.Text = yourNumber.ToString();
+            opponentLabel.Text = opponentNumber.ToString();
+            prizeLabel.Text = prizes[prizeIndex].ToString();
+            if (yourNumber > opponentNumber)
+            {
+                prizeLabel.ForeColor = Color.Red;
+                total += prizes[prizeIndex];
+            }
+            else
+            {
+                prizeLabel.ForeColor = Color.Gray;
+            }
+            label4.Text = "Your total prize: " + total.ToString();
+            label4.ForeColor = Color.Red;
+            scratchButton.Enabled = false;
+        }
     }
 }
