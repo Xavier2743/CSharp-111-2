57fb2f30c0cda54bfcf072d7f966d1ce312b03d7
Vic <h88001166@gmail.com>
2023-04-12 16:07:17 +0800

    Open a file and save it. Open a directory and show it.

--- Week08-01/Week08-01/Form1.Designer.cs
+++ Week08-01/Week08-01/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(46, 48);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Open";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(158, 48);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Save";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(46, 110);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(286, 296);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(418, 48);
+            button3.Name = "button3";
+            button3.Size = new Size(137, 23);
+            button3.TabIndex = 3;
+            button3.Text = "Show Directory";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(418, 110);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(280, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private Button button3;
+        private TextBox textBox1;
     }
 }
--- Week08-01/Week08-01/Form1.cs
+++ Week08-01/Week08-01/Form1.cs
@@ -2,9 +2,41 @@ namespace Week08_01
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog ofd = new OpenFileDialog();
+        SaveFileDialog sfd = new SaveFileDialog();
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofd.InitialDirectory = @"G:\Chrome";
+            ofd.Title = "Select a file";
+            ofd.Filter = "Image (*.jpg)|*.jpg|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sfd.InitialDirectory = @"G:\Chrome";
+            sfd.Title = "Save a file";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(sfd.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fbd.ShowDialog();
+            textBox1.Text = fbd.SelectedPath;
         }
     }
 }
