b7270569e341a85160e3d13b8b38294d59a81a98
Vic <h88001166@gmail.com>
2023-04-26 17:26:48 +0800

    Timer

--- Week10-04/Week10-04/Form1.cs
+++ Week10-04/Week10-04/Form1.cs
@@ -2,9 +2,21 @@ namespace Week10_04
 {
     public partial class Form1 : Form
     {
+
+        private int tickCount;
+
         public Form1()
         {
             InitializeComponent();
+            tickCount = 0;
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tickCount++;
+            label1.Text = tickCount.ToString();
         }
     }
 }
