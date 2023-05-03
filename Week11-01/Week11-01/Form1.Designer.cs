9f4f8e3502cac453bcc54e1c4bd33021db48dde8
Vic <h88001166@gmail.com>
2023-05-03 16:37:49 +0800

    Make a slot.

--- Week11-01/Week11-01/Form1.Designer.cs
+++ Week11-01/Week11-01/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(217, 92);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 150);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(341, 92);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 150);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(465, 92);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(100, 150);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(354, 273);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(279, 330);
+            label1.Name = "label1";
+            label1.Size = new Size(32, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Prize";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(341, 327);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
--- Week11-01/Week11-01/Form1.cs
+++ Week11-01/Week11-01/Form1.cs
@@ -2,9 +2,71 @@ namespace Week11_01
 {
     public partial class Form1 : Form
     {
+        int num1;
+        int num2;
+        int num3;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            num1 = rnd.Next(1, 8);
+            num2 = rnd.Next(1, 8);
+            num3 = rnd.Next(1, 8);
+
+            pictureBox_Show(pictureBox1, num1);
+            pictureBox_Show(pictureBox2, num2);
+            pictureBox_Show(pictureBox3, num3);
+
+            if (num1 * num2 * num3 % 343 == 0)
+            {
+                textBox1.Text = "10000";
+            }
+            else if (num1 * num2 * num3 % 49 == 0)
+            {
+                textBox1.Text = "1000";
+            }
+            else if (num1 * num2 * num3 % 7 == 0)
+            {
+                textBox1.Text = "100";
+            }
+            else
+            {
+                textBox1.Text = "You lose.";
+            }
+        }
+
+        private void pictureBox_Show(PictureBox pictureBox, int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    pictureBox.Image = Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox.Image = Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox.Image = Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox.Image = Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox.Image = Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox.Image = Properties.Resources._6;
+                    break;
+                case 7:
+                    pictureBox.Image = Properties.Resources._7;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
