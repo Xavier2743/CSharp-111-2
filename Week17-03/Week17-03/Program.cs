c57f33db37e9a1edf982751b4d04de1a10ac20a0
Vic <h88001166@gmail.com>
2023-06-14 17:00:10 +0800

    Multi-thread

---
+++ Week17-03/Week17-03/Program.cs
@@ -0,0 +1,14 @@
+﻿Thread t1 = new Thread(MyBackgroundTask);
+t1.Start();
+for (int i = 0; i < 100; i++)
+{
+    Console.Write("A");
+}
+
+static void MyBackgroundTask()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Console.Write("B");
+    }
+}
